6a0bd097bda1ac0e9d192131bd66e183d17b6171
pie <returnpie@icloud.com>
2020-05-31 20:55:34 +0900

    twilio var change

--- src/utils/sendSMS.ts
+++ src/utils/sendSMS.ts
@@ -1,6 +1,6 @@
 import Twilio from "twilio";
 
-const twilioClient = Twilio(process.env.TWILIO_SID, process.env.TWILIO_TOKEN);
+const twilioClient = Twilio(process.env.TWILIO_ACCOUNT_SID, process.env.TWILIO_AUTH_TOKEN);
 
 export const sendSMS = (to: string, body: string) => {
   return twilioClient.messages.create({
